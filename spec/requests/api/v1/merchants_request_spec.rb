1356313236ea039ecae52c3a624754889f390175
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-25 17:21:39 -0600

    Added merchant show record endpoint spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe 'Merchants API ' do
 
 		expect(Merchant.count).to eq(3)
 	end
+
+	it 'can get a merchant by id' do 
+		id = create(:merchant).id
+
+		get "/api/v1/merchants/#{id}"
+
+		merchant = JSON.parse(response.body)
+
+		expect(response).to be_successful
+		expect(merchant["id"]).to eq(id)
+	end
 end
