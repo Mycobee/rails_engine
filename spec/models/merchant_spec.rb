b6f460f631cc75b772b2094b74ecbc99f1ab8854
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-27 15:27:53 -0600

    Added spec for Merchant.revenue_date

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,65 +15,65 @@ RSpec.describe Merchant, type: :model do
 		@invoice_1a = create(:invoice, merchant: @merchant_1, customer: @customer_1, created_at: 1.day.ago)
 		@item_1a = create(:item, merchant: @merchant_1, created_at: 1.day.ago)
 		@invoice_item_1a = create(:invoice_item, item: @item_1a, invoice: @invoice_1a, created_at: 1.day.ago)
-		@transaction_1a = create(:transaction, invoice: @invoice_1a)
-		@failed_transaction_1a = create(:transaction, invoice: @invoice_1a)
+		@transaction_1a = create(:transaction, invoice: @invoice_1a, created_at: 1.day.ago)
+		@failed_transaction_1a = create(:transaction, invoice: @invoice_1a, created_at: 1.day.ago)
 		
 	
 		@invoice_2a = create(:invoice, merchant: @merchant_2, customer: @customer_2, created_at: 2.day.ago)
 		@item_2a = create(:item, merchant: @merchant_2, created_at: 2.day.ago)
 		@invoice_item_2a = create(:invoice_item, item: @item_2a, invoice: @invoice_2a, created_at: 2.day.ago)
-		@transaction_2a = create(:transaction, invoice: @invoice_2a)
-		@failed_transaction_2a = create(:transaction, invoice: @invoice_2a)
+		@transaction_2a = create(:transaction, invoice: @invoice_2a, created_at: 2.day.ago)
+		@failed_transaction_2a = create(:transaction, invoice: @invoice_2a, created_at: 2.day.ago)
 
 		@invoice_2b = create(:invoice, merchant: @merchant_2, customer: @customer_2, created_at: 2.days.ago)
 		@item_2b = create(:item, merchant: @merchant_2, created_at: 2.days.ago)
 		@invoice_item_2b = create(:invoice_item, item: @item_2b, invoice: @invoice_2b, created_at: 2.days.ago)
-		@transaction_2b = create(:transaction, invoice: @invoice_2b)
-		@failed_transaction_2b = create(:transaction, invoice: @invoice_2b)
+		@transaction_2b = create(:transaction, invoice: @invoice_2b, created_at: 2.day.ago)
+		@failed_transaction_2b = create(:transaction, invoice: @invoice_2b, created_at: 2.day.ago)
 
 
 		@invoice_3a = create(:invoice, merchant: @merchant_3, customer: @customer_3, created_at: 3.days.ago)
 		@item_3a = create(:item, merchant: @merchant_3, created_at: 3.days.ago)
 		@invoice_item_3a = create(:invoice_item, item: @item_3a, invoice: @invoice_3a, created_at: 3.days.ago)
-		@transaction_3a = create(:transaction, invoice: @invoice_3a)
-		@failed_transaction_3a = create(:transaction, invoice: @invoice_3a)
+		@transaction_3a = create(:transaction, invoice: @invoice_3a, created_at: 3.days.ago)
+		@failed_transaction_3a = create(:transaction, invoice: @invoice_3a, created_at: 3.days.ago)
 
 		@invoice_3b = create(:invoice, merchant: @merchant_3, customer: @customer_3, created_at: 3.days.ago)
 		@item_3b = create(:item, merchant: @merchant_3, created_at: 3.days.ago)
 		@invoice_item_3b = create(:invoice_item, item: @item_3b, invoice: @invoice_3b, created_at: 3.days.ago)
-		@transaction_3b = create(:transaction, invoice: @invoice_3b)
-		@failed_transaction_3b = create(:transaction, invoice: @invoice_3b)
+		@transaction_3b = create(:transaction, invoice: @invoice_3b, created_at: 3.days.ago)
+		@failed_transaction_3b = create(:transaction, invoice: @invoice_3b, created_at: 3.days.ago)
 
 		@invoice_3c	= create(:invoice, merchant: @merchant_3, customer: @customer_3, created_at: 3.days.ago)
 		@item_3c = create(:item, merchant: @merchant_3, created_at: 3.days.ago)
 		@invoice_item_3c = create(:invoice_item, item: @item_3c, invoice: @invoice_3c, created_at: 3.days.ago)
-		@transaction_3c = create(:transaction, invoice: @invoice_3c)
-		@failed_transaction_3c = create(:transaction, invoice: @invoice_3c)
+		@transaction_3c = create(:transaction, invoice: @invoice_3c, created_at: 3.days.ago)
+		@failed_transaction_3c = create(:transaction, invoice: @invoice_3c, created_at: 3.days.ago)
 
 
 		@invoice_4a = create(:invoice, merchant: @merchant_4, customer: @customer_4, created_at: 4.days.ago)
 		@item_4a = create(:item, merchant: @merchant_4, created_at: 4.days.ago)
 		@invoice_item_4a = create(:invoice_item, item: @item_4a, invoice: @invoice_4a, created_at: 4.days.ago)
-		@transaction_4a = create(:transaction, invoice: @invoice_4a)
-		@failed_transaction_4a = create(:transaction, invoice: @invoice_4a)
+		@transaction_4a = create(:transaction, invoice: @invoice_4a, created_at: 4.days.ago)
+		@failed_transaction_4a = create(:transaction, invoice: @invoice_4a, created_at: 4.days.ago)
 
 		@invoice_4b = create(:invoice, merchant: @merchant_4, customer: @customer_4, created_at: 4.days.ago)
 		@item_4b = create(:item, merchant: @merchant_4, created_at: 4.days.ago)
 		@invoice_item_4b = create(:invoice_item, item: @item_4b, invoice: @invoice_4b, created_at: 4.days.ago)
-		@transaction_4b = create(:transaction, invoice: @invoice_4b)
-		@failed_transaction_4b = create(:transaction, invoice: @invoice_4b)
+		@transaction_4b = create(:transaction, invoice: @invoice_4b, created_at: 4.days.ago)
+		@failed_transaction_4b = create(:transaction, invoice: @invoice_4b, created_at: 4.days.ago)
 
 		@invoice_4c	= create(:invoice, merchant: @merchant_4, customer: @customer_4, created_at: 4.days.ago)
 		@item_4c = create(:item, merchant: @merchant_4, created_at: 4.days.ago)
 		@invoice_item_4c = create(:invoice_item, item: @item_4c, invoice: @invoice_4c, created_at: 4.days.ago)
-		@transaction_4c = create(:transaction, invoice: @invoice_4c)
-		@failed_transaction_4c = create(:transaction, invoice: @invoice_4c)
+		@transaction_4c = create(:transaction, invoice: @invoice_4c, created_at: 4.days.ago)
+		@failed_transaction_4c = create(:transaction, invoice: @invoice_4c, created_at: 4.days.ago)
 
 		@invoice_4d	= create(:invoice, merchant: @merchant_4, customer: @customer_4, created_at: 4.days.ago)
 		@item_4d = create(:item, merchant: @merchant_4, created_at: 4.days.ago)
 		@invoice_item_4d = create(:invoice_item, item: @item_4d, invoice: @invoice_4d, created_at: 4.days.ago)
-		@transaction_4d = create(:transaction, invoice: @invoice_4d)
-		@failed_transaction_4d = create(:transaction, invoice: @invoice_4d)
+		@transaction_4d = create(:transaction, invoice: @invoice_4d, created_at: 4.days.ago)
+		@failed_transaction_4d = create(:transaction, invoice: @invoice_4d, created_at: 4.days.ago)
 	end
 
 	describe 'relationships' do
@@ -89,6 +89,12 @@ RSpec.describe Merchant, type: :model do
 
 		it '.most_items' do
 			expect(Merchant.most_items(3)).to eq([@merchant_4, @merchant_3, @merchant_2])	
+		end
+
+		it '.revenue_date' do
+			actual = Merchant.revenue_date(@transaction_4a.created_at.to_s(:db))
+			expected = ((@invoice_item_4a.unit_price * 2) + (@invoice_item_4b.unit_price * 2) + (@invoice_item_4c.unit_price * 2) + (@invoice_item_4d.unit_price * 2))
+			expect(actual).to eq(expected)
 		end
 	end
 end
