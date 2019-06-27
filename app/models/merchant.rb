3d30b8a2c397272bcfa5d32a89f97b6d972c061f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-27 15:12:00 -0600

    Added merchant most_items method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,5 +13,11 @@ class Merchant < ApplicationRecord
 						.limit(limit)
 		end
 
-		
+		def self.most_items(limit)
+						 joins('INNER JOIN items ON items.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.item_id = items.id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id')
+						.select('SUM(invoice_items.quantity) AS total_sold, merchants.*')
+						.group(:id)
+						.order('total_sold DESC')
+						.limit(limit)
+		end
 end
