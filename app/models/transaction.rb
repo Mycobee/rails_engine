af43ff6024f3ce191bbc67a8beaee60d266c2477
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 16:03:56 -0600

    Added Transaction.random class method functionality

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,9 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 	
 	scope :successful, -> { where(result: "success") }
+	
+	def self.random
+			rand_id = Transaction.all.pluck(:id).sample
+			Transaction.find(rand_id)
+	end
 end
