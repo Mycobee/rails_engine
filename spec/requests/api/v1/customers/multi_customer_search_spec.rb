fa47f6109f84fb97eed75292079b77cf75be2678
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 16:29:30 -0600

    Added find all endpoint spec for Customer

---
+++ spec/requests/api/v1/customers/multi_customer_search_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe 'Search Controller' do
+	before :each do
+		@customer_1 = create(:customer, created_at: '2019-06-23 14:53:59 UTC', updated_at: '2019-06-23 14:53:59 UTC')
+		@customer_2 = create(:customer, created_at: '2019-06-23 14:53:59 UTC', updated_at: '2019-06-23 14:53:59 UTC')
+  end	
+
+	it 'finds all customer by created_at' do
+		get "/api/v1/customers/find_all?created_at=2019-06-23-14:53:59-UTC"
+
+		expect(response).to be_successful
+
+		customer = JSON.parse(response.body)
+		expect(customer["data"].count).to eq(2)
+	end
+
+	it 'finds all customer by updated_at' do
+		get "/api/v1/customers/find_all?updated_at=2019-06-23-14:53:59-UTC"
+
+		expect(response).to be_successful
+
+		customer = JSON.parse(response.body)
+
+		expect(customer["data"].count).to eq(2)
+	end
+
+	it 'finds all customer by last name' do
+		get "/api/v1/customers/find_all?last_name=#{@customer_1.last_name}"
+
+		expect(response).to be_successful
+
+		customer = JSON.parse(response.body)
+	  	
+		expect(customer["data"].count).to eq(1)
+	end
+
+	it 'finds all customer by first name' do
+		get "/api/v1/customers/find_all?first_name=#{@customer_1.first_name}"
+
+		expect(response).to be_successful
+
+		customer = JSON.parse(response.body)
+	  	
+		expect(customer["data"].count).to eq(1)
+	end
+
+	it 'finds all customer by id' do
+		get "/api/v1/customers/find_all?id=#{@customer_1.id}"
+
+		expect(response).to be_successful
+
+		customer = JSON.parse(response.body)
+
+		expect(customer["data"].count).to eq(1)
+	end
+end
