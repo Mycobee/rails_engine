6e4b0e89fe55f945d0aa75f6deb7119996ee3569
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 11:17:05 -0600

    Added random merchant spec for record endpoint

--- spec/requests/api/v1/merchants/single_merchant_search_spec.rb
+++ spec/requests/api/v1/merchants/single_merchant_search_spec.rb
@@ -4,4 +4,15 @@ describe 'Search Controller' do
 	it 'finds a merchant by ID' do
 
 	end
+
+	it 'finds a random merchant' do
+		create_list(:merchant, 3)
+
+		get 'api/v1/merchants/random'
+
+		expect(response).to be_successful
+
+		merchant = JSON.parse(response.body)
+		expect(merchant["data"]["attributes"]).to have_key("id", "name")
+	end
 end
