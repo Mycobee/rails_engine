08fdf62a7371d4758574ac3656263de74da6e4bc
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 15:27:41 -0600

    completed .random functionality

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,9 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+	def self.random
+			rand_id = InvoiceItem.all.pluck(:id).sample
+			InvoiceItem.find(rand_id)
+	end
 end
