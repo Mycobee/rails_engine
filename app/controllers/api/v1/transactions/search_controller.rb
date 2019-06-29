c7056136c96cf2cc1d43ac7e52d49d5f31594629
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 15:59:43 -0600

    Added single transaction search functionality

--- app/controllers/api/v1/transactions/search_controller.rb
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -1,12 +1,12 @@
-class Api::V1::Merchants::SearchController < ApplicationController
+class Api::V1::Transactions::SearchController < ApplicationController
 	def show
-		render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
+		render json: TransactionSerializer.new(Transaction.find_by(merchant_params))
 	end
 
 	private
 
 	def merchant_params
-		params.permit(:id, :name, :updated_at, :created_at)
+		params.permit(:id, :result, :credit_card_number, :credit_card_expiration_date, :updated_at, :created_at)
 	end
 end
 
