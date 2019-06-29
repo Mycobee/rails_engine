e1335e2b83471743243567e7b6be93b4de1b0ae0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-29 16:02:28 -0600

    Added transaction.random spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Transaction, type: :model do
 	describe 'relationships' do 
 		it { should belong_to :invoice }
 	end
+	
+	describe 'class methods' do
+		it '.random' do
+			create_list(:transaction, 3)
+			expect(Transaction.random.class).to eq(Transaction)
+		end
+	end
 end
